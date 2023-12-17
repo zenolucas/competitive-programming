21b1474a3e2ac312576150776ce47d65131d900e
Zen Lucas <2200501@slu.edu.ph>
2023-12-17 18:31:09 +0800

    finished MexicanWave.java

--- MexicanWave.java
+++ MexicanWave.java
@@ -1,9 +1,9 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class MexicanWave {
     public static void main(String[] args) {
-            String[] stringArray = wave("a       b    ");
+            String[] stringArray = wave("a     b    ");
 
             for (String word : stringArray)
             {
@@ -12,24 +12,34 @@ public class MexicanWave {
     }
 
     public static String[] wave(String str) {
-        String[] stringArray = new String[str.length()];
-
+        ArrayList<String> arrayList = new ArrayList<>();
 
         if (Objects.equals(str, ""))
         {
-            return stringArray;
+            return new String[]{};
         }
 
         char[] charArray = str.toCharArray();
         for (int i = 0; i < str.length(); i++)
         {
-            charArray[i] = Character.toUpperCase(charArray[i]);
-            stringArray[i] = String.valueOf(charArray);
-            // then revert charArray back to lowercase
-            charArray[i] = Character.toLowerCase(charArray[i]);
+            // if character is a not a space
+            if (!Character.toString(charArray[i]).equals(" ")) {
+                // make the current 'wave' character uppercase
+                charArray[i] = Character.toUpperCase(charArray[i]);
+                // add string sequence w/ 1 uppercase char to arraylist
+                arrayList.add(String.valueOf(charArray));
+                // then revert charArray back to lowercase
+                charArray[i] = Character.toLowerCase(charArray[i]);
+            }
+        }
+
+        // transfer contents of arrayList<string> to string array
+        String[] stringArray = new String[arrayList.size()];
+        for (int i = 0; i < arrayList.size(); i++)
+        {
+            stringArray[i] = arrayList.get(i);
         }
 
         return stringArray;
     }
-
 }
