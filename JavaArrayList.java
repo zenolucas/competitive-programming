75167388d16dc3389197156afdde3aa8a219d9d2
Zen Lucas <2200501@slu.edu.ph>
2023-12-17 18:39:38 +0800

    refactor

--- JavaArrayList.java
+++ JavaArrayList.java
@@ -4,40 +4,32 @@ import java.util.Scanner;
 public class JavaArrayList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // inputs
+        // accept problem inputs
         int n = scanner.nextInt();
         ArrayList<ArrayList<Integer>> mainArrayList = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             ArrayList<Integer> arrayList = new ArrayList<>();
             int j = scanner.nextInt();
 
-            for (int k = 0; k < j; k++)
-            {
+            for (int k = 0; k < j; k++) {
                 arrayList.add(scanner.nextInt());
             }
             mainArrayList.add(arrayList);
         }
 
-        // queries
+        // for the queries part of the problem
         int q = scanner.nextInt();
+        int x; // index of line to get digit
+        int y; // index of digit in line
 
-        int x;
-        int y;
-
-        for (int i = 0; i < q; i++)
-        {
+        for (int i = 0; i < q; i++) {
             x = scanner.nextInt();
             y = scanner.nextInt();
-            try
-            {
+            try {
                 System.out.println(mainArrayList.get(x - 1).get(y - 1));
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("ERROR!");
             }
         }
-
     }
 }
