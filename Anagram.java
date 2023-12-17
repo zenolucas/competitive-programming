be08de902e557518083f8687d0a78cc9a3f2ed57
Zen Lucas <2200501@slu.edu.ph>
2023-12-17 18:34:54 +0800

    refactor

--- Anagram.java
+++ Anagram.java
@@ -27,7 +27,7 @@ public class Anagram {
         // Complete the function
         ArrayList<Character> arrayList = new ArrayList<>();
 
-        // populate distinct let1ters in arrayList
+        // populate distinct letters in arrayList
         for (int i = 0; i < a.length(); i++)
         {
             char letter = a.charAt(i);
